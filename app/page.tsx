35b1510fa2c017e177add498af61db33c6f9dacf
kareem <kareem.abdelnabi.22@gmail.com>
2025-04-11 14:30:39 +0200

    Project Setup

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="min-h-screen flex justify-center items-center bg-gray-100">
+      <h1 className="text-4xl font-bold text-gray-800">
+        GOE EgyBook
+      </h1>
+    </div>
+  );
+}
